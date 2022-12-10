699e2a1c3b6935e09295bca21490a839206c6ebe
butebaliyeva <utebaliyevabotakoz@gmail.com>
2022-12-11 00:25:06 +0600

    CsvSource

--- src/test/java/guru/qa/WikiWebTest.java
+++ src/test/java/guru/qa/WikiWebTest.java
@@ -1,15 +1,33 @@
 package guru.qa;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class WikiWebTest {
-    @Test
-    void googleTest(){
 
-        open ("https://www.wikipedia.org/");
+    @BeforeEach
+    void setUp() {
+        open("https://www.wikipedia.org/");
+    }
+
+    @CsvSource(value = {
+            "Acer, Acer",
+            "air astana, Air Astana"
+    })
+    @ParameterizedTest(name = "Проверка наличия заголовка {1}" +
+            " в результатах выдачи Википедии по запросу {0}")
+    @Tags({@Tag("BLOCKER"), @Tag("FEATURE")})
+    void wikiSearchTest(String searchQuery, String expectedLabel) {
+        $("#searchInput").setValue(searchQuery).pressEnter();
+        $(".mw-page-title-main").shouldHave(text(expectedLabel));
 
     }
+
 }
